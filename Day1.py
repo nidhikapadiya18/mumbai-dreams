c1cf23c58adf6cda552933f806bd4488ed32cb14
nidhikapadiya18 <nidhikapadiya8@gmail.com>
2026-05-23 21:19:54 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,25 @@
+# 21:18 - Nidhi Kapadiya ka pehla commit - DAY 1 DONE
+print("="*50)
+print("  MUMBAI-DREAMS PROJECT: OFFICIALLY STARTED")
+print("="*50)
+
+name = "Nidhi Kapadiya"
+age = 18
+college = "IT Student"
+from_city = "Vadodara"
+to_city = "Mumbai"
+day = 1
+skill_unlocked = "GitHub + Python"
+syros_fund = 500
+bf = None
+motivation = "∞"
+
+print(f"\nDeveloper: {name}, {age}")
+print(f"Mission: {from_city} → {to_city}")
+print(f"Day {day}: {skill_unlocked} ✅")
+print(f"Syros Fund: ₹{syros_fund}")
+print(f"Ex-BF: {bf}")
+print(f"Motivation Level: {motivation}")
+print("\nMumbai Progress: 20% Loading... ⚡")
+print("Message: Heartbreak compiled into code ❤️")
+print("="*50)
